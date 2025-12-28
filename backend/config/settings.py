c62182fc03fd7c560d2fb3cc1c319506b4b06e49
Jecson <jecsonrv3@gmail.com>
2025-12-28 11:32:55 -0600

    fix(settings): allow configuring AWS S3 region via env vars

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,7 +34,7 @@ if not DEBUG and os.getenv('AWS_ACCESS_KEY_ID'):
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')
     AWS_S3_SIGNATURE_VERSION = 's3v4'
     AWS_DEFAULT_ACL = None  # Files are private by default
     
